fc883fae026af4d70fe9c52ad781f9bf6f685a98
Dmitry <5460700+wolfeeg@users.noreply.github.com>
2022-06-24 17:13:32 +0500

    Update cube.js

--- one/cube.js
+++ one/cube.js
@@ -4,7 +4,7 @@
 // CubeCloud users by default.  Please contact support if you need them
 // enabled for your account.  You are still allowed to require
 // @cubejs-backend/*-driver packages.
-//66
+//77
 
 module.exports = {
 };
